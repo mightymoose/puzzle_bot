77d1100898617a3c03b0af4b252f7a0ad3ce1c76
Ryan Morris <mryan.morris@gmail.com>
2022-09-02 13:41:39 -0500

    feat(styles): add the tailwind css library

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,8 @@ config :puzzle_bot, PuzzleBotWeb.Endpoint,
   secret_key_base: "0+ynAP/qyvvcmdhDTHT4xcFMzTaODwR2ukP64aUloGCrvDgrqeZAq2lgGNCwan2D",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
